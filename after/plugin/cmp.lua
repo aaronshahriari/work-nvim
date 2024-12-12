247c7307d7d368e15a776f496d78676d4e432d7e
Aaron Shahriari <44651626+aaronshahriari@users.noreply.github.com>
2024-12-12 04:36:53 -0500

    Update cmp.lua with snippets

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,6 +1,11 @@
 local cmp = require "cmp"
 
 cmp.setup {
+    snippet = {
+        expand = function(args)
+            require 'luasnip'.lsp_expand(args.body)
+        end
+    },
     mapping = {
         ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
         ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
